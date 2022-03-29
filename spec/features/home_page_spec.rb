50762c07d0f00178aec8e59f4528e69a88c5f806
Troy Joslyn <tjjmoto@gmail.com>
2022-03-29 01:13:05 +0000

    unit tests added and failures fixed

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,16 +5,4 @@ RSpec.feature "Visiting the homepage", type: :feature do
     visit root_path
     expect(page).to have_text("Projects")
   end
-  scenario "The visitor should see Home" do
-    visit root_path
-    expect(page).to have_text("Home")
-  end
-  scenario "The visitor should see Troy Joslyn" do
-    visit root_path
-    expect(page).to have_text("Troy Joslyn")
-  end
-  scenario "The visitor should see navbar" do
-    visit root_path
-    expect(page).to have_xpath("//nav")
-  end
 end
